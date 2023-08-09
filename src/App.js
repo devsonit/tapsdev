f671dd669de638439be34a30d5d6e7eef82bbca6
abhishek <uic.16mca8231@gmail.com>
2023-08-09 13:24:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
 function App() {
+  const PayPalButton = window.paypal.Buttons.driver("react", {
+    React,
+    ReactDOM,
+  });
+
+  function _createOrder(data, actions) {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: "1",
+          },
+        },
+      ],
+    });
+  }
+  async function _onApprove(data, actions) {
+    let order = await actions.order.capture();
+    console.log(order);
+    return order;
+  }
+  function _onError(err) {
+    console.log(err);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PayPalButton
+        createOrder={(data, actions) => _createOrder(data, actions)}
+        onApprove={(data, actions) => _onApprove(data, actions)}
+        onCancel={() => _onError("Canceled")}
+        onError={(err) => _onError(err)}
+        style={{ layout: "horizontal", tagline: "false", marginHorizontal: 20 }}
+      />
     </div>
   );
 }
-
 export default App;

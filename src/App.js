fd0b2c78175b059e83aa57a0b994855b6f927e8a
abhishek <uic.16mca8231@gmail.com>
2023-08-09 13:48:16 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,39 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
 function App() {
-  const PayPalButton = window.paypal.Buttons.driver("react", {
-    React,
-    ReactDOM,
-  });
-
-  function _createOrder(data, actions) {
-    return actions.order.create({
-      purchase_units: [
-        {
-          amount: {
-            value: "1",
-          },
-        },
-      ],
-    });
-  }
-  async function _onApprove(data, actions) {
-    let order = await actions.order.capture();
-    console.log(order);
-    return order;
-  }
-  function _onError(err) {
-    console.log(err);
-  }
   return (
     <div className="App">
-      <PayPalButton
+      demo
+      {/* <PayPalButton
         createOrder={(data, actions) => _createOrder(data, actions)}
         onApprove={(data, actions) => _onApprove(data, actions)}
         onCancel={() => _onError("Canceled")}
         onError={(err) => _onError(err)}
         style={{ layout: "horizontal", tagline: "false", marginHorizontal: 20 }}
-      />
+      /> */}
     </div>
   );
 }
